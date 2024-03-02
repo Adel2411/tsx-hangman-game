6fbbff2e9b4aff46b0f8f5bc9953cde08e98d803
Adel2411 <hadjarabadel.2411@gmail.com>
2024-03-02 22:25:13 +0100

    finished to draw hangman shapes

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,1 +1,28 @@
+import './App.css'
+import {useState} from "react";
+import wordList from "./wordList.json"
+import {HangmanDraw} from "./Components/HangmanDraw.tsx";
+import {HangmanWord} from "./Components/HangmanWord.tsx";
+import {HangmanKeyboard} from "./Components/HangmanKeyboard.tsx";
 
+function App() {
+  const [wordToGuess, setWordToGuess] = useState<string>(() => {
+    return wordList[Math.floor(Math.random() * wordList.length)];
+  });
+  const [gessedletters, setGesssedletters] = useState<string[]>([]);
+
+  console.log(wordToGuess)
+
+  return (
+      <div className="App">
+          <div className="win-lose">Lose Win</div>
+          <HangmanDraw/>
+          <HangmanWord/>
+          <HangmanKeyboard/>
+      </div>
+  )
+
+
+}
+
+export default App
